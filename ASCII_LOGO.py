2e0852989be0d15c2ef246362dda243ecbef7e12
Tom And Jerry <111493279+MR-NAYIM-404@users.noreply.github.com>
2023-10-25 13:14:10 +0600

    Create ASCII_LOGO.py

---
+++ ASCII_LOGO.py
@@ -0,0 +1,6 @@
+import os
+Print('FOLLOW MY GITHUB')
+os.system('xdg-open https://github.com/MR-NAYIM-404')
+print('FOLLOW MY FB ID')
+os.system('xdg-open https://www.facebook.com/TODR.ABBU.LAGI?mibextid=ZbWKwL')
+import LOGO
